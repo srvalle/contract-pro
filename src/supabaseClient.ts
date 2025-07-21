668ea662b7f5cf103c5ef48785ccd509db28e508
Sergio <srvalle@gmail.com>
2025-07-21 14:02:23 -0300

    Move Supabase keys to .env and update supabaseClient.ts

--- src/supabaseClient.ts
+++ src/supabaseClient.ts
@@ -1,6 +1,6 @@
 import { createClient } from '@supabase/supabase-js';
 
-const supabaseUrl = 'https://ulbldzbfzyqghnhuyaiz.supabase.co';
-const supabaseAnonKey = 'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6InVsYmxkemJmenlxZ2huaHV5YWl6Iiwicm9sZSI6ImFub24iLCJpYXQiOjE3NTI1MDQ2NzAsImV4cCI6MjA2ODA4MDY3MH0.yXJDo0sIrNiE4lpBh3K4QmRYa7HA_M79YbmAt7QzqSM';
+const supabaseUrl = import.meta.env.VITE_SUPABASE_URL;
+const supabaseAnonKey = import.meta.env.VITE_SUPABASE_ANON_KEY;
 
 export const supabase = createClient(supabaseUrl, supabaseAnonKey);
